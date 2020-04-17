715c18830ab227d3fbb58ca0b15fc152d71dfd85
R0yo <msergeeva@iflex.ru>
2020-04-17 15:51:27 +0300

    добавила команду регистрации(без обработки)

--- t_bot.py
+++ t_bot.py
@@ -17,6 +17,22 @@ async def start_cmd_handler(message: types.Message):
     await message.reply(text)
 
 
+@dp.message_handler(commands='registration')
+async def registration_cmd_handler(message: types.Message):
+    text = 'для начала выбери город'
+    keyboard_markup = types.InlineKeyboardMarkup(row_width=2)
+
+    text_and_data = (
+        ('Moscow', 'msk'),
+        ('Saint Petersburg', 'spb'),
+    )
+
+    row_btns = (types.InlineKeyboardButton(text, callback_data=data) 
+                for text, data in text_and_data)
+    keyboard_markup.row(*row_btns)
+    await message.reply(text,  reply_markup=keyboard_markup)
+
+
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
 
