e85f64d2afa57a893651b1684a72dbd924ee0d47
R0yo <msergeeva@iflex.ru>
2020-05-02 18:52:49 +0300

    перенесла обработку сообщений в bot_api

--- bot_api.py
+++ bot_api.py
@@ -1,5 +1,6 @@
 from abc import ABC, abstractmethod
 from pg_api import PgAPI
+from aiogram import Bot, Dispatcher, executor, types
 
 
 class BotAPI(ABC):
@@ -19,6 +20,14 @@ class BotAPI(ABC):
     def get_event(self, data):
         pass
 
+    @abstractmethod
+    def process_message(self, data):
+        pass
+
+    @abstractmethod
+    def set_subsciber(self, data):
+        pass
+
 
 class TelegramAPI(BotAPI):
     def __init__(self, config):
@@ -27,7 +36,7 @@ class TelegramAPI(BotAPI):
     def set_db(self, config):
         """config = {database: str, user: str[, password: str]}"""
         self.db = PgAPI(**config)
-        
+
     @staticmethod
     def convert_data(data):
         """Метод для преобразования сообщения из Telegram
@@ -46,3 +55,37 @@ class TelegramAPI(BotAPI):
     def get_event(self, data):
         converted_data = self.convert_data(data)
         self.db.get_event(converted_data)
+
+    def process_message(self, message):
+        # TODO что там с обработкой данных convert_data ??
+        bot_commands = {
+            'start': 'start_command',
+            'registration': 'registration_command'
+        }
+        func = bot_commands.get(message.get_command(), lambda: "Я пока не знаю такой команды")
+        func(message)
+
+    def start_command(self, data):
+        text = 'привет, я бот, который подскажет, куда тебе сходить ' \
+               'в свободное время, для начала расскажи о себе ' \
+               '/registration ^^ '
+
+        self.set_subscriber(data.from_user)
+
+    @staticmethod
+    def registration_command(data):
+        text = 'Выбери город'
+        keyboard_markup = types.InlineKeyboardMarkup(row_width=2)
+        text_and_data = (
+            ('Moscow', 'msk'),
+            ('Saint Petersburg', 'spb'),
+        )
+        row_btns = (types.InlineKeyboardButton(text, callback_data=data)
+                    for text, data in text_and_data)
+        keyboard_markup.row(*row_btns)
+
+        return text, keyboard_markup
+
+    def set_subscriber(self, data):
+        # TODO нужна функци добавления инфы о подписке в бд
+        return 0
--- t_bot.py
+++ t_bot.py
@@ -1,6 +1,7 @@
 import logging
 from aiogram import Bot, Dispatcher, executor, types
 from config import TG_TOKEN
+from bot_api import TelegramAPI
 
 logging.basicConfig(level=logging.INFO)
 
@@ -8,30 +9,11 @@ bot = Bot(token=TG_TOKEN)
 dp = Dispatcher(bot)
 
 
-@dp.message_handler(commands='start')
-async def start_cmd_handler(message: types.Message):
-    text = 'привет, я бот, который подскажет, куда тебе сходить ' \
-           'в свободное время, для начала расскажи о себе ' \
-           '/registration ^^ '
-    await message.reply(text)
-
-
-@dp.message_handler(commands='registration')
-async def registration_cmd_handler(message: types.Message):
-    text = 'для начала выбери город'
-    keyboard_markup = types.InlineKeyboardMarkup(row_width=2)
-
-    text_and_data = (
-        ('Moscow', 'msk'),
-        ('Saint Petersburg', 'spb'),
-    )
-
-    row_btns = (types.InlineKeyboardButton(text, callback_data=data) 
-                for text, data in text_and_data)
-    keyboard_markup.row(*row_btns)
-    await message.reply(text,  reply_markup=keyboard_markup)
+# @dp.message_handler(commands=['start','registration'])
+async def command_cmd_handler(message: types.Message):
+    response, keyboard_markup = TelegramAPI.process_message(message)
+    await message.reply(response, reply_markup=keyboard_markup)
 
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
-
